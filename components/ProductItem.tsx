1b1d451e6b0aa4a80f144a5ca7974bd9efbcddc3
nicolas-kruger <nicolas.nkruger@gmail.com>
2022-01-13 13:17:34 -0300

    bundle analyzer

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -2,7 +2,7 @@
 import dynamic from "next/dynamic";
 import { FC, memo, useState } from "react";
 import { AddProductToWishListProps } from "./AddProductToWishList";
-
+import lodash from "lodash";
 const AddProductToWishList = dynamic<AddProductToWishListProps>(() => {
     return import("./AddProductToWishList").then(mod => mod.AddProductToWishList)
 }, {
@@ -39,4 +39,4 @@ export const ProductItemComponent: FC<ProductItemProps> = ({ product, onAddToWis
     )
 }
 
-export const ProductItem = memo(ProductItemComponent, ({ product: a }, { product: b }) => Object.is(a, b))
+export const ProductItem = memo(ProductItemComponent, ({ product: a }, { product: b }) => lodash.isEqual(a, b))
