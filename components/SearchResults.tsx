c66b5833ca0ec74d5b074a40c2322297042f90f9
nicolas-kruger <nicolas.nkruger@gmail.com>
2022-01-13 11:59:12 -0300

    virtualizacao

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,5 +1,6 @@
 import { FC, useMemo } from "react"
 import { ProductItem } from "./ProductItem"
+import { List, ListRowRenderer } from "react-virtualized";
 
 type SearchResultProps = {
     result: {
@@ -14,19 +15,31 @@ type SearchResultProps = {
 
 export const SearchResult: FC<SearchResultProps> = ({ result, onAddToWishList, totalPrice }) => {
 
+    const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+        return (
+            <div
+                key={key}
+                style={style}
+            >
+                <ProductItem
+                    product={result[index]}
+                    onAddToWishList={onAddToWishList}
+                />
+            </div>
+        )
+    }
 
     return (
         <div>
             <h2>{totalPrice}</h2>
-            {result.map((product, index) => {
-                return (
-                    <ProductItem
-                        key={product.id}
-                        product={product}
-                        onAddToWishList={onAddToWishList}
-                    />
-                )
-            })}
+            <List
+                height={300}
+                rowHeight={30}
+                width={900}
+                overscanRowCount={5}
+                rowCount={result.length}
+                rowRenderer={rowRenderer}
+            />
         </div>
     )
 }
